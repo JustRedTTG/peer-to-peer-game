3fbf26e2f4ab06814a0c6fd9c690292e002ebca5
red <red@redttg.com>
2024-03-17 16:08:27 +0200

    update some minor environmental things

--- server/server.py
+++ server/server.py
@@ -12,7 +12,7 @@ class Server(SecurityWrapper):
     def __init__(self):
         self.clients = {}
 
-        super().__init__(os.environ.get('SERVER_HOST', 'localhost'), int(os.environ.get('SERVER_PORT', '9024')))
+        super().__init__('localhost', 9024)
         print(f'Server: {self.host}:{self.port}')
 
     def _receive(self, data, addr):
